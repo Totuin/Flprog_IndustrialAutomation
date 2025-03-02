5070e173844177fe6d6f34a05be7efcab5fa1cc1
Totuin <117037443+Totuin@users.noreply.github.com>
2025-03-02 13:02:18 +0500

    02-03-25

--- src/flprogHourMeter.cpp
+++ src/flprogHourMeter.cpp
@@ -1,1 +1,60 @@
 #include "flprogHourMeter.h"
+
+void FLProgHourMeter::equipmentStatus(bool status)
+{
+    if ((status) && (!_equipmentStatus))
+    {
+        _startTime = millis();
+    }
+    if ((!status) && (_equipmentStatus) && (_callBack != 0))
+    {
+        _callBack(_workSecondsCounter);
+    }
+    _equipmentStatus = status;
+}
+
+void FLProgHourMeter::pool()
+{
+    if (!_equipmentStatus)
+    {
+        return;
+    }
+    if (!RT_HW_Base.getIsTimerUs(_startTime, 1000))
+    {
+        return;
+    }
+    _startTime = millis();
+    _workSecondsCounter++;
+    calculateSecondsMinutsHours();
+}
+
+void FLProgHourMeter::calculateSecondsMinutsHours()
+{
+    uint32_t oldHours = _hours;
+    uint32_t temp;
+    _hours = (uint32_t)(_workSecondsCounter / 3600);
+    temp = _workSecondsCounter - (_hours * 3600);
+    _minutes = (uint8_t)(temp / 60);
+    _seconds = temp - (_minutes * 60);
+    if (_callBack == 0)
+    {
+        return;
+    }
+    if (oldHours == _hours)
+    {
+        return;
+    }
+    _callBack(_workSecondsCounter);
+}
+
+void FLProgHourMeter::setWrkSeconds(uint32_t value)
+{
+    _workSecondsCounter = value;
+    calculateSecondsMinutsHours();
+}
+
+void FLProgHourMeter::resetCounter()
+{
+    _workSecondsCounter = 0;
+    calculateSecondsMinutsHours();
+}
--- src/flprogHourMeter.h
+++ src/flprogHourMeter.h
@@ -2,8 +2,30 @@
 #include <Arduino.h>
 #include "flprogUtilites.h"
 
+typedef void (*FLProgHourMeterCallback)(uint32_t counter);
 class FLProgHourMeter
 {
 public:
+    FLProgHourMeter() {};
+
+    void equipmentStatus(bool status);
+    void setWrkSeconds(uint32_t value);
+    void resetCounter();
+    void setCallBack(FLProgHourMeterCallback func) { _callBack = func; };
+    uint32_t workTimeAsSeconds() { return _workSecondsCounter; };
+    uint8_t seconds() { return _seconds; };
+    uint8_t minutes() { return _minutes; };
+    uint32_t hours() { return _hours; };
+
+    void pool();
+
 protected:
+    void calculateSecondsMinutsHours();
+    FLProgHourMeterCallback _callBack = 0;
+    bool _equipmentStatus = false;
+    uint32_t _workSecondsCounter = 0;
+    uint32_t _startTime;
+    uint8_t _seconds = 0;
+    uint8_t _minutes = 0;
+    uint32_t _hours = 0;
 };
